f1a82bab2d5374e39d474e2fbb2b24092b57e2bb
Chris Pollard <cspollard@gmail.com>
2023-07-15 12:46:16 +0100

    adding requirements

--- setup.py
+++ setup.py
@@ -4,4 +4,16 @@ setup \
   ( name='jax-conditional-flows'
   , version='0.1'
   , py_modules=['models']
+  , install_requires= \
+    [ "numpy"
+    , "scipy"
+    , "matplotlib"
+    , "jax"
+    , "flax"
+    , "distrax"
+    , "optax"
+    , "einops"
+    , "corner"
+    , "tqdm"
+    ]
   )
